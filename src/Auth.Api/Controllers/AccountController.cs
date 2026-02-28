f82c6e7309020c9c6e8ae9f5410dd85f3a1330de
artem <a.kurochkin@sigma-it.ru>
2026-02-28 15:17:16 +0300

    feat: register remove

--- src/Auth.Api/Controllers/AccountController.cs
+++ src/Auth.Api/Controllers/AccountController.cs
@@ -9,12 +9,12 @@ namespace Auth.Api.Controllers;
 [Route("api/account")]
 public sealed class AccountController(IAuthService authService, ITwoFactorAuthService twoFactorAuthService) : ControllerBase
 {
-    [HttpPost("register")]
-    [AllowAnonymous]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
-    public async Task<ActionResult<UserDto>> Register([FromBody] RegisterRequest request, CancellationToken cancellationToken) =>
-        Ok(await authService.RegisterAsync(request, cancellationToken));
+    // [HttpPost("register")]
+    // [AllowAnonymous]
+    // [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    // [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+    // public async Task<ActionResult<UserDto>> Register([FromBody] RegisterRequest request, CancellationToken cancellationToken) =>
+    //     Ok(await authService.RegisterAsync(request, cancellationToken));
 
     [HttpPost("2fa/enable")]
     [Authorize]
